183eb6c2e91ba2d650fc31aeff6fd6b60aef4009
Sean Oughton <seanoughton@gmail.com>
2018-01-29 22:27:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,3 +51,13 @@ def sum_array(array)
         sum + number
     end
 end
+
+def add_s(array)
+  array.each_with_index.collect do |word,index|
+        if index != 1
+          word = word + "s"
+        else
+          word = word
+        end
+  end
+end
