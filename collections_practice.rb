b25326ca1919a056ee2b1df96115e588f623f720
Sean Oughton <seanoughton@gmail.com>
2018-01-29 22:41:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,68 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|x,y| y<=>x}
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[1..2] = array[1..2].sort do |a,b|
+    if a==b || a<b || a>b
+      1
+    end
+  end
+  array
+end
+
+def swap_elements_from_to(array, index, new_index)
+  array[index], array[new_index] = array[new_index], array[index]
+  return array
+end
+
+def reverse_array(array)
+  new_array = array.reverse
+end
+
+def kesha_maker(array)
+  return_array = []
+  array.each do |string|
+    string[2] = "$"
+    return_array << string
+  end
+  return_array
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+    array.inject do |sum,number|
+        sum + number
+    end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word,index|
+        if index != 1
+          word = word + "s"
+        else
+          word = word
+        end
+  end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,12 @@ describe 'collections practice' do
     end
   end
 
+  describe 'swap_elements_from_to' do
+  it 'swaps elements and allows you to specify the index of the element you would like to move to a new index' do
+    expect(swap_elements_from_to(["one", "two", "three"], 2, 1)).to eq(["one", "three", "two"])
+  end
+end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
