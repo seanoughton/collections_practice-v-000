2b3ffc090881d42a9f2f953756ea478ef4c0fd09
Sean Oughton <seanoughton@gmail.com>
2018-01-29 20:32:53 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,7 +19,5 @@ def sort_array_char_count(array)
 end
 
 def swap_elements_from_to(array)
-  #Build a method swap_elements that takes in an array and swaps the second and third elements. Remember that array indices start at 0, so the second element has an index of 1 and the third element has an index of 2.
-  array.sort do |a,b|
-  end
+
 end
