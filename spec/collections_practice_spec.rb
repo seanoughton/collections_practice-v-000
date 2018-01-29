1f2ec2bd483980c2d809bb00798ac95ec40beaaf
Sean Oughton <seanoughton@gmail.com>
2018-01-29 22:39:49 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -32,7 +32,7 @@ describe 'collections practice' do
 
   describe 'swap_elements_from_to' do
   it 'swaps elements and allows you to specify the index of the element you would like to move to a new index' do
-    expect(swap_elements_from_to(["one", "two", "three"], 2, 1)).to eq(["one", "three", "two"])
+    expect(swap_elements_from_to(["one", "two", "three"]).to eq(["one", "three", "two"])
   end
 end
 
